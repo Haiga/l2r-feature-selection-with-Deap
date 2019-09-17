33ffa096c6a79e7ae07ddf974b1b22b67085abc1
pedro_rga <pedro_rga@hotmail.com>
2019-09-17 20:04:26 -0300

    Evaluate Individuo com SUB_CROSS validation

--- evaluateIndividuo.py
+++ evaluateIndividuo.py
@@ -1,11 +1,12 @@
 import l2rCodes
 import numpy as np
+from sklearn import model_selection
+import cudf
 
-
-def getEval(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_test, ENSEMBLE, NTREES, SEED,
+def getEval(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_train, ENSEMBLE, NTREES, SEED,
             DATASET, METRIC, NUM_FOLD, ALGORITHM):
     evaluation = []
-    ndcg, queries = getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_test,
+    ndcg, queries = getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_train,
                                            ENSEMBLE, NTREES, SEED, DATASET,
                                            METRIC)
 
@@ -28,9 +29,9 @@ def getWeights(params):
     return weights
 
 
-def getPrecision(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_test, ENSEMBLE, NTREES, SEED, DATASET,
+def getPrecision(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_train, ENSEMBLE, NTREES, SEED, DATASET,
                  METRIC):
-    ndcg, queries = getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_test,
+    ndcg, queries = getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_train,
                                            ENSEMBLE, NTREES, SEED, DATASET,
                                            METRIC)
     return ndcg
@@ -42,7 +43,6 @@ def getTotalFeature(individuo):
 
 # PRECISA SER CORRIGIDA SE HOUVER MAIS DE UM BASEINE
 def getRisk(queries, DATASET, NUM_FOLD, ALGORITHM):
-
     base = []
 
     arq = open(r'./baselines/' + DATASET + '/Fold' + NUM_FOLD + '/' + ALGORITHM + '.txt')
@@ -57,7 +57,7 @@ def getRisk(queries, DATASET, NUM_FOLD, ALGORITHM):
     return r
 
 
-def getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_test, ENSEMBLE, NTREES,
+def getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_test, query_id_train, ENSEMBLE, NTREES,
                            SEED, DATASET,
                            METRIC):
     list_mask = list(individuo)
@@ -65,21 +65,51 @@ def getPrecisionAndQueries(individuo, NUM_GENES, X_train, y_train, X_test, y_tes
     for i in range(NUM_GENES):
         if list_mask[i] == '1':
             features.append(i)
-    # X_train_ind = X_train[:, features]
+    X_train_ind = X_train[:, features]
     # X_test_ind = X_test[:, features]
 
-    X_train_ind = X_train.iloc[:, features]
-    X_test_ind = X_test.iloc[:, features]
+    queriesList = l2rCodes.getQueries(query_id_train)
+    scoreTrain = [0] * len(y_train)
 
-    scoreTest = [0] * len(y_test)
-    model = l2rCodes.getTheModel(ENSEMBLE, NTREES, 0.3, SEED, DATASET)
-    model.fit(X_train_ind, y_train)
-    # resScore = model.predict(X_test_ind)
-    resScore = model.predict(X_test_ind).to_array()
-    c = 0
-    for i in resScore:
-        scoreTest[c] = i
-        c = c + 1
+    kf = model_selection.KFold(n_splits=3, shuffle=True, random_state=SEED)
+    XF_train_index = []
+    XF_test_index = []
 
-    ndcg, queries = l2rCodes.getEvaluation(scoreTest, query_id_test, y_test, DATASET, METRIC, "test")
+    for qtrain_index, qtest_index in kf.split(queriesList):
+
+        del XF_train_index[:]
+        del XF_test_index[:]
+
+        for qtr in qtrain_index:
+            XF_train_index = XF_train_index + queriesList[qtr]
+        for qts in qtest_index:
+            XF_test_index = XF_test_index + queriesList[qts]
+
+        XF_train, XF_test = X_train_ind[XF_train_index], X_train_ind[XF_test_index]####
+        yf_train, yf_test = y_train[XF_train_index], y_train[XF_test_index]
+        # qf_train, qf_test = query_id_train[XF_train_index], query_id_train[XF_test_index]
+
+        # resScore = None
+        model = l2rCodes.getTheModel(ENSEMBLE, NTREES, 0.3, SEED, DATASET)
+        ##
+        XF_train = cudf.DataFrame.from_records(XF_train)
+        XF_test = cudf.DataFrame.from_records(XF_test)
+        yf_train = cudf.Series(yf_train)
+        ##
+        model.fit(XF_train, yf_train)
+        resScore = model.predict(XF_test)
+
+        # sortRespRel=[rel for (score, rel) in sorted(zip(resScore,yf_test), key=lambda pair: pair[0], reverse=True)]
+        c = 0
+        for i in XF_test_index:
+            scoreTrain[i] = resScore[c]
+            c = c + 1
+    ndcg, queries = l2rCodes.getEvaluation(scoreTrain, query_id_train, y_train, DATASET, METRIC, "test")
     return ndcg, queries
+
+
+
+    # X_train_ind = X_train.iloc[:, features]
+    # X_test_ind = X_test.iloc[:, features]
+
+
