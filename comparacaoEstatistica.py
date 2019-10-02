1dba8e117e0b96559caa0f902fd0d0f9d85453c2
Pedro Rodrigues <pedro_rga@hotmail.com>
2019-10-02 20:13:38 -0300

    Update comparacaoEstatistica.py

--- comparacaoEstatistica.py
+++ comparacaoEstatistica.py
@@ -8,15 +8,4 @@ def getTTestR(x_vet, y_vet):
     rvtest = robjects.r['t.test']
     pvalue = rvtest(rd1, rd2, paired=True)[2][0]
 
-    # print rvtest(rd1, rd2, paired=True)
-
-    # Usando o próprio python para calcular t-test
-    # line = str(rvtest(rd1, rd2, paired=True))
-    # text= line.split("\n")
-    # pvalue= text[-6]
-    # pvalue = rvtest(rd1, rd2, paired=True)
-    # print "[", pvalue, "]"
-    # print np.mean(x_vet), np.mean(y_vet), pvalue
-    # pvalue = rvtest(rd1, rd2, paired=True)
-
     return pvalue
